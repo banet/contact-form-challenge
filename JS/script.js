c8f4468df551ee697cfcba66de8830ade5d86a8a
Bane Tepavcevic <bane.tepavcevic@gmail.com>
2025-11-12 15:35:23 +0100

    JS required filed is achomplished!

--- JS/script.js
+++ JS/script.js
@@ -11,10 +11,43 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("JS fajl se učitao! ✅");
 
   const form = document.querySelector("form");
-  console.log("Forma:", form);
+  form.addEventListener("submit", handleSubmit);
 
-  form.addEventListener("submit", function (event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log("Forma je uspešno poslata!");
-  });
+    console.log("Forma je poslata");
+    validateForm(form);
+  }
+
+  function validateForm(form) {
+    console.log("Provera vam formu");
+
+    const requiredFields = form.querySelectorAll("input[required], textarea[required]");
+    
+    requiredFields.forEach(field => {
+      // ukloni stari error ako postoji
+      const existingError = field.parentElement.querySelector(".error-message");
+      if (existingError) existingError.remove();
+
+      // ako polje nema vrednost
+      if (!field.value.trim()) {
+        showErrorMessage(field, "This field is required");
+        field.style.borderBottom = "2px solid #d93025";
+      } else {
+        field.style.borderBottom = "1px solid #ccc";
+      }
+    });
+  }
+
+  function showErrorMessage(field, message) {
+    const error = document.createElement("span");
+    error.classList.add("error-message");
+    error.textContent = message;
+    error.style.color = "#d93025";
+    error.style.fontSize = "0.9rem";
+    error.style.display = "block";
+    error.style.marginTop = "4px";
+
+    field.parentElement.appendChild(error);
+  }
 });
